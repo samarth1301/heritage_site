01712c7992303adb868d5d29860c0b53d2b0c663
samarth1301 <samarthsingh890.ss@gmail.com>
2022-06-18 17:53:54 +0530

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import {AiOutlineMenu} from "react-icons/ai";
 
-const Navbar = () => {
+export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-cyan-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              to={"/"}
+            >
+              Our Heritage 
+            </Link>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <AiOutlineMenu/>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex gap-4 flex-col lg:flex-row list-none lg:ml-auto">
+            
+              <li className="nav-item text-white">
+               <Link to={"/login"}>
+                Login
+               </Link>
+              </li>
+              <li className="nav-item text-white">
+               <Link to={"/signup"}>
+                Sign Up
+               </Link>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
-
-export default Navbar
