f3e101c5cf80bbf980c1fe2d915113c519f515a5
samarth1301 <samarthsingh890.ss@gmail.com>
2022-06-18 17:36:40 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
 function App() {
   return (
-   <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-   </>
+    <>
+      <Router>
+        
+        <Routes>
+            <Route path="/" element={<h1>home</h1>} />
+            <Route path="/login" element={<h1>login</h1>} />
+            <Route path="/signup" element={<h1>signup</h1>} />
+         
+
+
+          <Route path="*" element={<h1 className="text-3xl font-bold underline">
+            Not Found
+          </h1>} />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
